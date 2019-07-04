35a81213958dcda1c4b15a765a4db00cd5f56598
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-07-04 13:41:32 -0700

    add images and styling to about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,7 +4,21 @@ import './About.css';
 function About() {
   return (
     <div className="aboutContainer">
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta accusamus velit rerum tempora dolor provident pariatur officiis reiciendis eligendi ab. Quasi architecto dignissimos nostrum quis amet vitae porro, sequi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolore quibusdam aspernatur, quo cum mollitia placeat est provident dolores deserunt nisi autem nam blanditiis asperiores adipisci repellendus, consectetur debitis. Alias. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente provident praesentium ducimus asperiores aut nobis voluptates doloremque. Quam aperiam repellendus, facilis asperiores quos qui beatae nulla eos cum, nobis id!</p>
+      <hr></hr>
+      <div className="fb-row">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta accusamus velit rerum tempora dolor provident pariatur officiis reiciendis eligendi ab. Quasi architecto dignissimos nostrum quis amet vitae porro, sequi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolore quibusdam aspernatur, quo cum mollitia placeat est provident dolores deserunt nisi autem nam blanditiis asperiores adipisci repellendus, consectetur debitis. Alias.</p>
+      <img className="floatLeft" src={require('./../../assets/beer1.jpg')} alt="ipa"/>
+      </div>
+      <hr></hr>
+      <div className="fb-row">
+      <img className="floatRight" src={require('./../../assets/beer2.jpg')} alt="stout"/>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta accusamus velit rerum tempora dolor provident pariatur officiis reiciendis eligendi ab. Quasi architecto dignissimos nostrum quis amet vitae porro, sequi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolore quibusdam aspernatur, quo cum mollitia placeat est provident dolores deserunt nisi autem nam blanditiis asperiores adipisci repellendus, consectetur debitis. Alias.</p>
+      </div>
+      <hr></hr>
+      <div className="fb-row">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta accusamus velit rerum tempora dolor provident pariatur officiis reiciendis eligendi ab. Quasi architecto dignissimos nostrum quis amet vitae porro, sequi debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos dolore quibusdam aspernatur, quo cum mollitia placeat est provident dolores deserunt nisi autem nam blanditiis asperiores adipisci repellendus, consectetur debitis. Alias.</p>
+      <img className="floatLeft" src={require('./../../assets/beer3.jpeg')} alt="cider"/>
+      </div>
     </div>
   );
 }

d13d811a267501e9be03ae8d224fc322367db00e
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-07-04 09:18:26 -0700

    add content to navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import './css/Navigation.css';
 
 export class Navigation extends Component {
   render() {
     return (
       <div>
-        
+        <ul>
+          <li className="navInline"><a href="/">Darrion's Taproom</a></li>
+          <li className="navInline">About Us</li>
+          <li className="navInline">Tap Selection</li>
+          <li className="navInline">Snacks</li>
+        </ul>
       </div>
     )
   }

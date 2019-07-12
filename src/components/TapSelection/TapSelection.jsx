e17eda79a02150f137873cdfa09792afefee1766
Darrion Gering <darrionkg@Darrions-MacBook-Pro.local>
2019-07-12 10:36:10 -0700

    refactor and edit sort functions

--- src/components/TapSelection/TapSelection.jsx
+++ src/components/TapSelection/TapSelection.jsx
@@ -23,16 +23,17 @@ function TapSelection() {
   }
 
   function sortKegsByAlcoholContent() {
-    this.kegs.sort((a, b) => a.alcoholContent-b.alcoholContent);
+    kegs = kegs.sort((a, b) => a.alcoholContent-b.alcoholContent);
+    console.log(kegs)
   }
   function sortKegsByPrice() {
-    this.kegs.sort((a, b) => a.price-b.price);
+    kegs = kegs.sort((a, b) => a.price-b.price);
   }
 
   return (
       <div className="beerList">
-        <button>Sort Kegs By Alcohol Content</button>
-        <button>Sort Kegs By Price</button>
+        <button onClick={sortKegsByAlcoholContent()}>Sort Kegs By Alcohol Content</button>
+        <button onClick={sortKegsByPrice()}>Sort Kegs By Price</button>
         {kegs.map((keg, index) =>
           <KegProp name={keg.name}
           brand={keg.brand}
